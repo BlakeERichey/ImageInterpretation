7b04d95e9468914e3e39a8012214edcc974d8255
Blake Richey <spsprogrammer@SPSs-Mac-mini.local>
2019-03-28 11:58:11 -0500

    Added 3 additional colors: Brown, Orange, Grey

--- ColorClassifier/ColorClassifier/Source.cpp
+++ ColorClassifier/ColorClassifier/Source.cpp
@@ -24,25 +24,44 @@ int main() {
 
 string classifyColor(int r, int g, int b) {
 	int currentArr[] = { r, g, b };	//color passed to function
-	string classes[] = { "red", "cyan", "blue", "black", "green", "white", "yellow", "magenta" };
+	string classes[] = { 
+    "red", 
+    "cyan", 
+    "blue",
+    "grey",
+    "black", 
+    "green", 
+    "white",
+    "brown",
+    "orange", 
+    "yellow", 
+    "magenta" 
+  };
 
 	int red[]     = { 255,   0,   0};
 	int cyan[]    = {   0, 255, 255};
 	int blue[]    = {   0,   0, 255};
+  int grey[]    = { 128, 128, 128};
 	int black[]   = {   0,   0,   0};
 	int green[]   = {   0, 255,   0};
 	int white[]   = { 255, 255, 255};
+  int brown[]   = { 101,  67,  33};
+  int orange[]  = { 255, 165,   0};
 	int yellow[]  = { 255, 255,   0};
 	int magenta[] = { 255,   0, 255};
+
 
 	struct distance //distance from color array
 	{
 		int rd;
 		int cd;
 		int bd;
+    int grd;
 		int bkd;
 		int gd;
 		int wd;
+    int brd;
+    int od;
 		int yd;
 		int md;
 	};
@@ -51,20 +70,41 @@ string classifyColor(int r, int g, int b) {
 		distanceArr(currentArr, red,     3), 
 		distanceArr(currentArr, cyan,    3),
 		distanceArr(currentArr, blue,    3),
+    distanceArr(currentArr, grey,    3),
 		distanceArr(currentArr, black,   3),
 		distanceArr(currentArr, green,   3),
 		distanceArr(currentArr, white,   3),
+    distanceArr(currentArr, brown,   3),
+    distanceArr(currentArr, orange,  3),
 		distanceArr(currentArr, yellow,  3),
 		distanceArr(currentArr, magenta, 3),
 	};
 
 
 	//----------Find minimum distance----------
-	double dArr[] = { deltas.rd, deltas.cd, deltas.bd, deltas.bkd, deltas.gd, deltas.wd, deltas.yd, deltas.md };
+	double dArr[] = { 
+    deltas.rd, 
+    deltas.cd, 
+    deltas.bd,
+    deltas.grd, 
+    deltas.bkd, 
+    deltas.gd, 
+    deltas.wd,
+    deltas.brd,
+    deltas.od, 
+    deltas.yd, 
+    deltas.md 
+  };
+
+  // cout << "Display Distance Array" << endl;
+  // for(int index = 0; index < 11; index++){
+  //   cout << dArr[index] << " ";
+  // }
+  // cout << endl;
 
 	int classIndex = 0;	//index of closest color for classifier
 	double min = dArr[0];
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < 11; i++) {
 		if (dArr[i] < min) {
 			min = dArr[i];
 			classIndex = i;
